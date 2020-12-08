b549e327f5b2742f93091910ada8f65a3932e11e
akash prasher <prince.akash.prasher@gmail.com>
2020-12-08 20:52:50 +0530

    Adding 404 page not found
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+// Importing Pages
 import Home from "./components/pages/home";
 import About from "./components/pages/about";
 import Contact from "./components/pages/contact";
 import Navbar from "./components/layout/navbar";
+import NotFound from './components/pages/not-found';
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -16,6 +20,7 @@ function App() {
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/about" component={About}></Route>
           <Route exact path="/contact" component={Contact}></Route>
+          <Route component={NotFound}></Route>
         </Switch>
       </div>
     </Router>
