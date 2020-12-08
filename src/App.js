467577afa919f742df9a7e41abeed488166cd81e
akash prasher <prince.akash.prasher@gmail.com>
2020-12-08 19:55:59 +0530

    Importing component from react-router-dom
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./components/pages/home";
+import About from "./components/pages/about";
+import Contact from "./components/pages/contact";
+import Navbar from "./components/layout/navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Home />
+        <About />
+        <Contact />
+      </div>
+    </Router>
   );
 }
 
