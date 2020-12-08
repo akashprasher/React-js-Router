0b4def7c32d50995ca2cfa2ca4de4b861c1f574b
akash prasher <prince.akash.prasher@gmail.com>
2020-12-08 19:19:25 +0530

    Adding home.js
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

---
+++ src/components/pages/home.js
@@ -0,0 +1,13 @@
+import { React }  from 'react';
+
+const home = () => {
+    return(
+        <div className="container">
+            <div className="py-4">
+                <h1>Home Page</h1>
+            </div>
+        </div>
+    )
+};
+
+export default home;
