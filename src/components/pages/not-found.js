68cc1b6ff78d86a74708123720b9c87470842c41
akash prasher <prince.akash.prasher@gmail.com>
2020-12-09 13:16:37 +0530

    minor update(responsive)
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- src/components/pages/not-found.js
+++ src/components/pages/not-found.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="not-found">
-        <h1 className=" container display-1 font-weight-bold tp-m">Page Not Found - 404</h1><br />
-        <p>Go Back to <Link className="mr-3" to="/">/home</Link></p>
+        <h1 className="container display-1 font-weight-bold tp-m">Page Not Found - 404</h1>
+        <p className="mb-5 mt-5">Go Back to <Link className="mr-3" to="/">/home</Link></p>
     </div>
   );
 };
