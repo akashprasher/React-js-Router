f8acb52788e2b077216f025adef8a40eaa89acf2
akash prasher <prince.akash.prasher@gmail.com>
2020-12-09 11:07:32 +0530

    adding style and update not-found.js
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- src/components/pages/not-found.js
+++ src/components/pages/not-found.js
@@ -1,10 +1,11 @@
 import React from "react";
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className="not-found">
-        <h1 className=" container display-1 font-weight-bold">Page Not Found - 404</h1>
+        <h1 className=" container display-1 font-weight-bold tp-m">Page Not Found - 404</h1><br />
+        <p>Go Back to <Link className="mr-3" to="/">/home</Link></p>
     </div>
   );
 };
