71b2201dda623fde08eda6f479a43cd1b9967119
akash prasher <prince.akash.prasher@gmail.com>
2020-12-09 10:32:58 +0530

    Adding 404 and style it
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- src/components/pages/not-found.js
+++ src/components/pages/not-found.js
@@ -1,9 +1,10 @@
 import React from "react";
+// import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="container">
-        <h1 className="display-1">Page Not Found - 404</h1>
+    <div className="not-found">
+        <h1 className=" container display-1 font-weight-bold">Page Not Found - 404</h1>
     </div>
   );
 };
