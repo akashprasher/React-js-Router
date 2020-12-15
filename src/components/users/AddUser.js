70bcbed015d9a92da9c9065226c29c6c8bef05b7
akash prasher <prince.akash.prasher@gmail.com>
2020-12-15 15:42:54 +0530

    Adding Module for user Adding
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: "",
+  });
+
+  const { name, username, email, phone, website } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+
   return (
     <div className="container mt-5">
       <div className="border shadow p-3">
         <h4 className="text-center">Add New User</h4>
-        <form className="mt-3">
+        <form onSubmit={(e) => onSubmit(e)} className="mt-3">
           <div className="form-group">
             <input
               type="text"
               className="form-control form-control-lg"
               placeholder="Enter Your Name"
               name="name"
+              value={name}
+              onChange={(e) => onInputChange(e)}
               required
             />
           </div>
@@ -21,6 +46,8 @@ const AddUser = () => {
               className="form-control form-control-lg"
               placeholder="Enter Your Username"
               name="username"
+              value={username}
+              onChange={(e) => onInputChange(e)}
               required
             />
           </div>
@@ -30,6 +57,8 @@ const AddUser = () => {
               className="form-control form-control-lg"
               placeholder="Enter Your Email Address"
               name="email"
+              value={email}
+              onChange={(e) => onInputChange(e)}
               required
             />
           </div>
@@ -38,7 +67,9 @@ const AddUser = () => {
               type="text"
               className="form-control form-control-lg"
               placeholder="Enter Your Phone Number"
-              name="phone-number"
+              name="phone"
+              value={phone}
+              onChange={(e) => onInputChange(e)}
               required
             />
           </div>
@@ -48,6 +79,8 @@ const AddUser = () => {
               className="form-control form-control-lg"
               placeholder="Enter Your Website"
               name="website"
+              value={website}
+              onChange={(e) => onInputChange(e)}
               required
             />
           </div>
